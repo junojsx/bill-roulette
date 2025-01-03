925a38fe69fd3a69d5e05850fad695447c20c14f
junojsx <junojsx@gmail.com>
2025-01-03 14:07:14 -0800

    adding addname function and displaying name list

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const { createApp, reactive, computed } = Vue;
 
 const DEFAULT_STATE = {
-  state: false,
+  state: true,
   inputName:"",
   names: [],
   error: "",
@@ -13,9 +13,18 @@ createApp({
   setup(){
     const data = reactive(DEFAULT_STATE) //creating the data this way instead of creating the default state object inside the set up is cleaner.
 
+    //methods
+
+    const addNameToList = () => {
+      const userName= data.inputName;
+      data.names.push(userName)
+      return console.log(data.names)
+    }
 
     return {
-      data
+      //this is where you import all datas and methods from the setup.
+      data,
+      addNameToList
     }
   }
 }).mount("#app");
